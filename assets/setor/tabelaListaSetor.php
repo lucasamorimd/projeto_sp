5d50371bbe9d4599105eb1eb9303d255cb116c41
lucasamorimd <lucas.ad@hotmail.com>
2021-01-25 17:56:53 -0300

    Update tabelaListaSetor.php

--- assets/setor/tabelaListaSetor.php
+++ assets/setor/tabelaListaSetor.php
@@ -1,35 +1,35 @@
 <div class="table-responsive">
-                      <table class="table">
-                        <thead class=" text-primary">
-                          <th>
-                            ID
-                          </th>
-                          <th>
-                            Setor
-                          </th>
-                          <th>
-                            Coordenador
-                          </th>
-                          
-                        </thead>
-                        <tbody>
-                        	<?php foreach ($setor as $key) {?>
-                        		
-                          <tr>
-                            <td>
-                              <?php echo $key->id_setor;?>
-                            </td>                            
-                            <td>
-                              <?php echo $key->nome_setor;?>
-                            </td>
-                            <td>
-                              <?php echo $key->coordenador;?>
-                            </td>
+  <table class="table">
+    <thead class=" text-primary">
+      <th>
+        ID
+      </th>
+      <th>
+        Setor
+      </th>
+      <th>
+        Coordenador
+      </th>
+      
+    </thead>
+    <tbody>
+      <?php foreach ($setor as $key) {?>
+        
+        <tr>
+          <td>
+            <?php echo $key->id_setor;?>
+          </td>                            
+          <td>
+            <?php echo $key->nome_setor;?>
+          </td>
+          <td>
+            <?php echo $key->coordenador;?>
+          </td>
 
-                          </tr>
-                        	<?php }?>
+        </tr>
+      <?php }?>
 
 
-                        </tbody>
-                      </table>
-                    </div>
+    </tbody>
+  </table>
+</div>
