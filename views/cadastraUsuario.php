fbcd4279d3f9e0a298a733cca5d35e4dd2dfa29f
lucasamorimd <lucas.ad@hotmail.com>
2021-01-25 18:05:49 -0300

    Update cadastraUsuario.php

--- views/cadastraUsuario.php
+++ views/cadastraUsuario.php
@@ -1,16 +1,12 @@
-<?php header("Content-type: text/html; charset=utf-8",true); ?>
 <?php
 
 session_start();
 require_once '../vendor/listaSetor.php';
 require_once '../vendor/setdeSessao.php';
 if($matricula != 1234){
-header('Location: ../painel/index.php');
-die();   
+  header('Location: ../painel/index.php');
+  die();   
 }
-
-
-
 ?>
 
 <!--
@@ -30,91 +26,91 @@ The above copyright notice and this permission notice shall be included in all c
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-   <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
   
   <title>
    Patrimônios - Cadastro de Usuário
-  </title>
-  <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-  <!--     Fonts and icons     -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-  <!-- Material Kit CSS -->
-  <link href="../assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+ </title>
+ <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+ <!--     Fonts and icons     -->
+ <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+ <!-- Material Kit CSS -->
+ <link href="../assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
+ <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <!--<link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>-->
-  <style type="text/css">
-    .custom-select {
-      position: relative;
-      font-family: Arial;
-    }
+    <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>-->
+    <style type="text/css">
+      .custom-select {
+        position: relative;
+        font-family: Arial;
+      }
 
-    .custom-select select {
-      display: none; /*hide original SELECT element: */
-    }
+      .custom-select select {
+        display: none; /*hide original SELECT element: */
+      }
 
-    .select-selected {
-      background-color: DodgerBlue;
-    }
+      .select-selected {
+        background-color: DodgerBlue;
+      }
 
-    /* Style the arrow inside the select element: */
-    .select-selected:after {
-      position: absolute;
-      content: "";
-      top: 14px;
-      right: 10px;
-      width: 0;
-      height: 0;
-      border: 6px solid transparent;
-      border-color: #fff transparent transparent transparent;
-    }
+      /* Style the arrow inside the select element: */
+      .select-selected:after {
+        position: absolute;
+        content: "";
+        top: 14px;
+        right: 10px;
+        width: 0;
+        height: 0;
+        border: 6px solid transparent;
+        border-color: #fff transparent transparent transparent;
+      }
 
-    /* Point the arrow upwards when the select box is open (active): */
-    .select-selected.select-arrow-active:after {
-      border-color: transparent transparent #fff transparent;
-      top: 7px;
-    }
+      /* Point the arrow upwards when the select box is open (active): */
+      .select-selected.select-arrow-active:after {
+        border-color: transparent transparent #fff transparent;
+        top: 7px;
+      }
 
-    /* style the items (options), including the selected item: */
-    .select-items div,.select-selected {
-      color: #ffffff;
-      padding: 8px 16px;
-      border: 1px solid transparent;
-      border-color: transparent transparent rgba(0, 0, 0, 0.1) transparent;
-      cursor: pointer;
-    }
+      /* style the items (options), including the selected item: */
+      .select-items div,.select-selected {
+        color: #ffffff;
+        padding: 8px 16px;
+        border: 1px solid transparent;
+        border-color: transparent transparent rgba(0, 0, 0, 0.1) transparent;
+        cursor: pointer;
+      }
 
-    /* Style items (options): */
-    .select-items {
-      position: absolute;
-      background-color: DodgerBlue;
-      top: 100%;
-      left: 0;
-      right: 0;
-      z-index: 99;
-    }
+      /* Style items (options): */
+      .select-items {
+        position: absolute;
+        background-color: DodgerBlue;
+        top: 100%;
+        left: 0;
+        right: 0;
+        z-index: 99;
+      }
 
-    /* Hide the items when the select box is closed: */
-    .select-hide {
-      display: none;
-    }
+      /* Hide the items when the select box is closed: */
+      .select-hide {
+        display: none;
+      }
 
-    .select-items div:hover, .same-as-selected {
-      background-color: rgba(0, 0, 0, 0.1);
-    }
+      .select-items div:hover, .same-as-selected {
+        background-color: rgba(0, 0, 0, 0.1);
+      }
 
-  </style>
-</head>
+    </style>
+  </head>
 
-<body class="">
-  <?php include '../assets/navbar.php'; ?>
-      <!-- End Navbar -->
-      <div class="content">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-md-12">
-                <div class="puff-in-center">
+  <body class="">
+    <?php include '../assets/navbar.php'; ?>
+    <!-- End Navbar -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="puff-in-center">
               <div class="card">
                 <div class="card-header card-header-primary">
                   <h4 class="card-title">Perfil</h4>
@@ -143,54 +139,54 @@ The above copyright notice and this permission notice shall be included in all c
                       </div>
                     </div>
                     <div class="row">
-                       <div class="col-md-4">
-                        <div class="form-group">
-                          
-                      <label class="bmd-label-floating">Setor</label>
-                      <select class="custom-select" name="id_setor">
+                     <div class="col-md-4">
+                      <div class="form-group">
+                        
+                        <label class="bmd-label-floating">Setor</label>
+                        <select class="custom-select" name="id_setor">
                           <option selected value="">Selecione o Setor</option>
                           <?php
 
                           foreach ($setor as $key) {
                             # code...
-                          
-                          ?>
-                          <option value="<?php echo $key->id_setor; ?>"><?php echo $key->nome_setor; ?></option>
+                            
+                            ?>
+                            <option value="<?php echo $key->id_setor; ?>"><?php echo $key->nome_setor; ?></option>
                           <?php } ?>
-                      </select>
-                        </div>
-                       </div>
-                       <div class="col-md-4">
-                         <div class="form-group">
-                           <label class="bmd-label-floating">Perfil</label>
-                           <select class="custom-select" name="perfil">
-                             <option selected value="">Selecione o Tipo de Perfil</option>
-                             <option value="1">Gestor</option>
-                             <option value="2">Funcionário</option>
-                           </select>
-                         </div>
-                       </div>
-
+                        </select>
+                      </div>
                     </div>
+                    <div class="col-md-4">
+                     <div class="form-group">
+                       <label class="bmd-label-floating">Perfil</label>
+                       <select class="custom-select" name="perfil">
+                         <option selected value="">Selecione o Tipo de Perfil</option>
+                         <option value="1">Gestor</option>
+                         <option value="2">Funcionário</option>
+                       </select>
+                     </div>
+                   </div>
 
-                    <button type="submit" class="btn btn-primary">Cadastrar</button>
-                    <div class="clearfix">
-                      
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <footer class="footer">
+                 </div>
 
-      </footer>
-    </div>
-  </div>
-  
+                 <button type="submit" class="btn btn-primary">Cadastrar</button>
+                 <div class="clearfix">
+                  
+                 </div>
+               </form>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+ </div>
+ <footer class="footer">
+
+ </footer>
+</div>
+</div>
+
 <?php include_once '../assets/carregaJS.php'; ?>
 </body>
 
