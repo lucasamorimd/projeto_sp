e20ede79e1107430b3b7eabf00ae6c21c10c2247
lucasamorimd <lucas.ad@hotmail.com>
2021-01-25 17:53:59 -0300

    Update listagemPat.php

--- assets/patrimonio/listagemPat.php
+++ assets/patrimonio/listagemPat.php
@@ -17,113 +17,112 @@
     </th>
     <?php if($perfil == 1){?>
 
-    <th>Editar</th>
-    <th>Trocar</th>
-    <th>Realocações</th>
+      <th>Editar</th>
+      <th>Trocar</th>
+      <th>Realocações</th>
     <?php }else{ ?>
-    <th>Realocações</th>
+      <th>Realocações</th>
 
- <?php } ?>
+    <?php } ?>
   </thead>
- 
+
   <tbody>
     <?php foreach ($patri as $key) {
 
       ?>
       
-    <tr>
-      <td>
-        <?php echo $key->id_patrimonio;?>
-      </td>                            
-      <td>
-        <?php echo $key->nome_setor;?>
-      </td>
-      <td>
-        <?php echo $key->descricao;?>
-      </td>
-      <td>
-        <?php echo $key->unidade;?>
-      </td>
-      <td>
-        <?php echo $key->marca;?>
-      </td>
-      
+      <tr>
+        <td>
+          <?php echo $key->id_patrimonio;?>
+        </td>                            
+        <td>
+          <?php echo $key->nome_setor;?>
+        </td>
+        <td>
+          <?php echo $key->descricao;?>
+        </td>
+        <td>
+          <?php echo $key->unidade;?>
+        </td>
+        <td>
+          <?php echo $key->marca;?>
+        </td>
+
 
         <?php if($perfil == 1):?> <!-- SE FOR COORDENADOR LOGADO-->
         <td class="td-actions text-center">
           <a href="cadastraPatrimonio.php?patrimonio=<?php echo base64_encode($key->id_patrimonio);?>">
-            
-          <button type="button" rel="tooltip" title="Editar informações" class="btn btn-primary btn-link btn-sm">
-            <i class="material-icons">edit</i>
-          </button>
+
+            <button type="button" rel="tooltip" title="Editar informações" class="btn btn-primary btn-link btn-sm">
+              <i class="material-icons">edit</i>
+            </button>
           </a>
-          </td>
+        </td>
         <td class="td-actions text-center">
           <a href="cadastraTroca.php?patrimonio=<?php echo base64_encode($key->id_patrimonio);?>">
             <button type="button" rel="tooltip" title="Realizar troca" class="btn btn-danger btn-link btn-sm">
               <i class="material-icons">compare_arrows</i>
             </button></a>
-        </td>
-        <td class="td-actions text-center">
-          <a href="listaTrocas.php?trocasid=<?php echo base64_encode($key->id_patrimonio);?>">
-            <button type="button" rel="tooltip" title="Listagem de trocas" class="btn btn-danger btn-link btn-sm">
-              <i class="material-icons">library_books</i>
-            </button></a>
-        </td> 
-        <?php else:?> <!-- SE FOR FUNCIONÁRIO LOGADO -->
-          <td class="td-actions text-center">
-                <a href="listaTrocas.php?trocasid=<?php echo base64_encode($key->id_patrimonio);?>">
-            <button type="button" rel="tooltip" title="Listagem de trocas" class="btn btn-danger btn-link btn-sm">
-              <i class="material-icons">library_books</i>
-            </button></a>
           </td>
-        <?php endif;?>
+          <td class="td-actions text-center">
+            <a href="listaTrocas.php?trocasid=<?php echo base64_encode($key->id_patrimonio);?>">
+              <button type="button" rel="tooltip" title="Listagem de trocas" class="btn btn-danger btn-link btn-sm">
+                <i class="material-icons">library_books</i>
+              </button></a>
+            </td> 
+            <?php else:?> <!-- SE FOR FUNCIONÁRIO LOGADO -->
+            <td class="td-actions text-center">
+              <a href="listaTrocas.php?trocasid=<?php echo base64_encode($key->id_patrimonio);?>">
+                <button type="button" rel="tooltip" title="Listagem de trocas" class="btn btn-danger btn-link btn-sm">
+                  <i class="material-icons">library_books</i>
+                </button></a>
+              </td>
+            <?php endif;?>
 
-    </tr>
-    <?php }?>
+          </tr>
+        <?php }?>
 
 
-  </tbody>
-</table>
-  
+      </tbody>
+    </table>
 
-<?php if($p_a >= 2 && $p_a < $paginas ): ?>
-<ul class="pagination justify-content-end">
-    <li class="page-item">
-      <a class="page-link" href="listaPatrimonio.php?p=1>">Início</a>
-    </li>
-    <li class="page-item">
-      <a class="page-link" href="listaPatrimonio.php?p=<?=$anterior?>">Anterior</a>
-    </li>
-    <li class="page-item"><a class="page-link"><?php echo $p_a."/".$paginas;?></a></li>
-    <li class="page-item">
-      <a class="page-link" href="listaPatrimonio.php?p=<?=$proxima?>">Próxima</a>
-    </li>
-</ul>
 
-  <?php elseif($p_a == 1 && $paginas > 1): ?>
-<ul class="pagination justify-content-end">
-    <li class="page-item"><a class="page-link"><?php echo $p_a."/".$paginas;?></a></li>
-    <li class="page-item">
-    <li class="page-item">
-      <a class="page-link" href="listaPatrimonio.php?p=<?=$proxima?>">Próxima</a>
-    </li>
-</ul>
-    <?php elseif($p_a>1 && $p_a == $paginas): ?>
-<ul class="pagination justify-content-end">
-    <li class="page-item">
-      <a class="page-link" href="listaPatrimonio.php?p=1">Início</a>
-    </li>
-    
-    <li class="page-item">
-      <a class="page-link" href="listaPatrimonio.php?p=<?=$anterior?>">Anterior</a>
-    </li>
+    <?php if($p_a >= 2 && $p_a < $paginas ): ?>
+      <ul class="pagination justify-content-end">
+        <li class="page-item">
+          <a class="page-link" href="listaPatrimonio.php?p=1>">Início</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="listaPatrimonio.php?p=<?=$anterior?>">Anterior</a>
+        </li>
         <li class="page-item"><a class="page-link"><?php echo $p_a."/".$paginas;?></a></li>
-    <li class="page-item">
-    <li class="page-item disabled">
-      <a class="page-link" href="javascript:;" tabindex="-1">Próxima</a>
-    </li>
-</ul>
+        <li class="page-item">
+          <a class="page-link" href="listaPatrimonio.php?p=<?=$proxima?>">Próxima</a>
+        </li>
+      </ul>
 
-<?php endif;?>   
+      <?php elseif($p_a == 1 && $paginas > 1): ?>
+        <ul class="pagination justify-content-end">
+          <li class="page-item"><a class="page-link"><?php echo $p_a."/".$paginas;?></a></li>
+          <li class="page-item">
+            <li class="page-item">
+              <a class="page-link" href="listaPatrimonio.php?p=<?=$proxima?>">Próxima</a>
+            </li>
+          </ul>
+          <?php elseif($p_a>1 && $p_a == $paginas): ?>
+            <ul class="pagination justify-content-end">
+              <li class="page-item">
+                <a class="page-link" href="listaPatrimonio.php?p=1">Início</a>
+              </li>
 
+              <li class="page-item">
+                <a class="page-link" href="listaPatrimonio.php?p=<?=$anterior?>">Anterior</a>
+              </li>
+              <li class="page-item"><a class="page-link"><?php echo $p_a."/".$paginas;?></a></li>
+              <li class="page-item">
+                <li class="page-item disabled">
+                  <a class="page-link" href="javascript:;" tabindex="-1">Próxima</a>
+                </li>
+              </ul>
+
+            <?php endif;?> 
