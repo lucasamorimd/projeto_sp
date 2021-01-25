583470cf68f24e8c5bab891b3f6a1f10392b51d1
lucasamorimd <lucas.ad@hotmail.com>
2021-01-25 18:03:22 -0300

    Update cadastraSetor.php

--- views/cadastraSetor.php
+++ views/cadastraSetor.php
@@ -1,4 +1,3 @@
-<?php header("Content-type: text/html; charset=utf-8",true); ?>
 <?php
 
 require '../vendor/permissoes.php';
@@ -20,58 +19,58 @@ require '../vendor/permissoes.php';
   <link href="../assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <!--<link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>-->
-</head>
+    <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>-->
+  </head>
 
-<body>
-  
-  	<?php include '../assets/navbar.php'; ?>
+  <body>
+    
+    <?php include '../assets/navbar.php'; ?>
 
-        <!-- End Navbar -->
-        <div class="content">
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-md-12">
-                  <div class="puff-in-center">
-                <div class="card">
-                  <div class="card-header card-header-primary">
-                    <h4 class="card-title">Setor</h4>
-                    <p class="card-category">Dados de Cadastro</p>
-                  </div>
-                  <div class="card-body">
-                    
-            <form action="../controllers/setor/ctrl_setor.php" method="post">
-            	<div class="row">
-            	  <div class="col-md-3">
-            	    <div class="form-group">
-            	      <label class="bmd-label-floating">Nome do Setor</label>
-            	      <input type="text" class="form-control" name="setor">
-            	    </div>
-            	  </div>
-            	  <div class="col-md-5">
-            	    <div class="form-group">
-            	      <label class="bmd-label-floating">Coordenador(a)</label>
-            	      <input type="text" class="form-control" name="coordenador">
-            	    </div>
-            	  </div>
+    <!-- End Navbar -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="puff-in-center">
+              <div class="card">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title">Setor</h4>
+                  <p class="card-category">Dados de Cadastro</p>
+                </div>
+                <div class="card-body">
+                  
+                  <form action="../controllers/setor/ctrl_setor.php" method="post">
+                    <div class="row">
+                      <div class="col-md-3">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Nome do Setor</label>
+                          <input type="text" class="form-control" name="setor">
+                        </div>
+                      </div>
+                      <div class="col-md-5">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Coordenador(a)</label>
+                          <input type="text" class="form-control" name="coordenador">
+                        </div>
+                      </div>
 
-            	<button type="submit" class="btn btn-primary pull-right">Cadastrar</button>
-            	<div class="clearfix"></div>
-            </form>
+                      <button type="submit" class="btn btn-primary pull-right">Cadastrar</button>
+                      <div class="clearfix"></div>
+                    </form>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        </div>
-      </div>
-        <footer class="footer">
-
-        </footer>
       </div>
     </div>
-  </body>
+    <footer class="footer">
+
+    </footer>
+  </div>
+</div>
+</body>
 <?php include_once '../assets/carregaJS.php'; ?>
 
-  </html>      
+</html>      
