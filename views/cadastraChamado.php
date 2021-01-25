ec5db1f17c665dd843c5cd17e6c3a19feda50af3
lucasamorimd <lucas.ad@hotmail.com>
2021-01-25 18:01:26 -0300

    Update cadastraChamado.php

--- views/cadastraChamado.php
+++ views/cadastraChamado.php
@@ -22,114 +22,114 @@ require_once'../vendor/listaSetor.php';
   <link href="../assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <!--<link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>-->
-  <style type="text/css">
-  	.custom-select {
-  	  position: relative;
-  	  font-family: Arial;
-  	}
+    <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>-->
+    <style type="text/css">
+      .custom-select {
+        position: relative;
+        font-family: Arial;
+      }
 
-  	.custom-select select {
-  	  display: none; /*hide original SELECT element: */
-  	}
+      .custom-select select {
+        display: none; /*hide original SELECT element: */
+      }
 
-  	.select-selected {
-  	  background-color: DodgerBlue;
-  	}
+      .select-selected {
+        background-color: DodgerBlue;
+      }
 
-  	/* Style the arrow inside the select element: */
-  	.select-selected:after {
-  	  position: absolute;
-  	  content: "";
-  	  top: 14px;
-  	  right: 10px;
-  	  width: 0;
-  	  height: 0;
-  	  border: 6px solid transparent;
-  	  border-color: #fff transparent transparent transparent;
-  	}
+      /* Style the arrow inside the select element: */
+      .select-selected:after {
+        position: absolute;
+        content: "";
+        top: 14px;
+        right: 10px;
+        width: 0;
+        height: 0;
+        border: 6px solid transparent;
+        border-color: #fff transparent transparent transparent;
+      }
 
-  	/* Point the arrow upwards when the select box is open (active): */
-  	.select-selected.select-arrow-active:after {
-  	  border-color: transparent transparent #fff transparent;
-  	  top: 7px;
-  	}
+      /* Point the arrow upwards when the select box is open (active): */
+      .select-selected.select-arrow-active:after {
+        border-color: transparent transparent #fff transparent;
+        top: 7px;
+      }
 
-  	/* style the items (options), including the selected item: */
-  	.select-items div,.select-selected {
-  	  color: #ffffff;
-  	  padding: 8px 16px;
-  	  border: 1px solid transparent;
-  	  border-color: transparent transparent rgba(0, 0, 0, 0.1) transparent;
-  	  cursor: pointer;
-  	}
+      /* style the items (options), including the selected item: */
+      .select-items div,.select-selected {
+        color: #ffffff;
+        padding: 8px 16px;
+        border: 1px solid transparent;
+        border-color: transparent transparent rgba(0, 0, 0, 0.1) transparent;
+        cursor: pointer;
+      }
 
-  	/* Style items (options): */
-  	.select-items {
-  	  position: absolute;
-  	  background-color: DodgerBlue;
-  	  top: 100%;
-  	  left: 0;
-  	  right: 0;
-  	  z-index: 99;
-  	}
+      /* Style items (options): */
+      .select-items {
+        position: absolute;
+        background-color: DodgerBlue;
+        top: 100%;
+        left: 0;
+        right: 0;
+        z-index: 99;
+      }
 
-  	/* Hide the items when the select box is closed: */
-  	.select-hide {
-  	  display: none;
-  	}
+      /* Hide the items when the select box is closed: */
+      .select-hide {
+        display: none;
+      }
 
-  	.select-items div:hover, .same-as-selected {
-  	  background-color: rgba(0, 0, 0, 0.1);
-  	}
+      .select-items div:hover, .same-as-selected {
+        background-color: rgba(0, 0, 0, 0.1);
+      }
 
-  </style>
-</head>
+    </style>
+  </head>
 
-<body>
-        
-          <?php include '../assets/navbar.php'; ?>
-      
-  	
+  <body>
+    
+    <?php include '../assets/navbar.php'; ?>
+    
+    
 
-        <!-- End Navbar -->
-        <div class="content">
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-md-12">
-                  <div class="puff-in-center">
-                <div class="card">
-                  <div class="card-header card-header-primary">
-                    <h4 class="card-title">Chamado</h4>
-                    <p class="card-category">Dados da Solicitação</p>
-                  </div>
-                  <div class="card-body">
-
-                    <?php include_once '../assets/chamado/formCadastraChamado.php';?>
-                  </div>
+    <!-- End Navbar -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="puff-in-center">
+              <div class="card">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title">Chamado</h4>
+                  <p class="card-category">Dados da Solicitação</p>
                 </div>
+                <div class="card-body">
+
+                  <?php include_once '../assets/chamado/formCadastraChamado.php';?>
                 </div>
               </div>
-               </div>
             </div>
           </div>
         </div>
-        <footer class="footer">
-          <div class="container-fluid">
-            <nav class="float-left">
-              <ul>
-
-              </ul>
-            </nav>
-
-           
-          </div>
-        </footer>
       </div>
     </div>
-   <!-- <script type="text/javascript" src="../lib/jssocket/websocket.js"></script>-->
-    <?php include_once '../assets/carregaJS.php'; ?>
-<?php include_once '../lib/jssocket/websocket.php';?>
-  </body>
+  </div>
+  <footer class="footer">
+    <div class="container-fluid">
+      <nav class="float-left">
+        <ul>
 
-  </html>      
+        </ul>
+      </nav>
+
+      
+    </div>
+  </footer>
+</div>
+</div>
+<!-- <script type="text/javascript" src="../lib/jssocket/websocket.js"></script>-->
+<?php include_once '../assets/carregaJS.php'; ?>
+<?php include_once '../lib/jssocket/websocket.php';?>
+</body>
+
+</html>      
