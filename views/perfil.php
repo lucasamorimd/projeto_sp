07c95ff4e18b2f18e235e68e1b8f2fb7869b8ab7
lucasamorimd <lucas.ad@hotmail.com>
2021-01-25 18:14:55 -0300

    Update perfil.php

--- views/perfil.php
+++ views/perfil.php
@@ -1,6 +1,4 @@
 <?php
-ini_set ( 'display_errors' , 1); 
-error_reporting (E_ALL);
 session_start();
 require_once '../vendor/setdeSessao.php';
 require_once '../vendor/listaSetor.php';
@@ -28,47 +26,47 @@ The above copyright notice and this permission notice shall be included in all c
 <head>
   <meta charset="utf-8" />
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-   <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
- Patrimônios - Perfil
-  </title>
-  <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-  <!--     Fonts and icons     -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-  <!-- Material Kit CSS -->
-  <link href="../assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+   Patrimônios - Perfil
+ </title>
+ <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+ <!--     Fonts and icons     -->
+ <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+ <!-- Material Kit CSS -->
+ <link href="../assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
+ <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
  <!-- <link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>-->
 </head>
 
 <body class="">
   <?php include '../assets/navbar.php'; ?>
-      <!-- End Navbar -->
-      <div class="content">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-md-12">
-                <div class="fade-in-fwd">
-              <div class="card">
-                <div class="card card-profile card-header card-header-primary">
+  <!-- End Navbar -->
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="fade-in-fwd">
+            <div class="card">
+              <div class="card card-profile card-header card-header-primary">
                 <div class="card-avatar">
-                    <?php if($key->foto == null):?>
+                  <?php if($key->foto == null):?>
                     
                     <a href="../views/perfil.php">
                       
                       <img class="img" src="../assets/img/faces/perfil-blog.png" />
-                      </a>
+                    </a>
                     <?php else:?>
-                  <a href="../views/perfil.php">
-                      
-                      <img class="img fotoperfil" src="../assets/img/faces/<?php echo $key->foto;?>" />
+                      <a href="../views/perfil.php">
+                        
+                        <img class="img fotoperfil" src="../assets/img/faces/<?php echo $key->foto;?>" />
                       </a>
-                      <?php endif;?>
-                      </div>
-                      
+                    <?php endif;?>
+                  </div>
+                  
                   <h4 class="card-title">Perfil</h4>
                   <p class="card-category">Dados de Cadastro</p>
                   
@@ -110,39 +108,39 @@ The above copyright notice and this permission notice shall be included in all c
                       </div>
                     </div>
                     <div class="row">
-                    	<?php if($key->perfil == 1): ?>
-                      <div class="col-md-12">
-                        <div class="form-group">
-                          <label class="bmd-label-floating">Tipo de Perfil</label>
-                          <input type="text" class="form-control" value="Coordenador" disabled="">
+                      <?php if($key->perfil == 1): ?>
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <label class="bmd-label-floating">Tipo de Perfil</label>
+                            <input type="text" class="form-control" value="Coordenador" disabled="">
+                          </div>
+                        </div>
+                        <?php else:?>
+                          <div class="col-md-12">
+                            <div class="form-group">
+                              <label class="bmd-label-floating">Tipo de Perfil</label>
+                              <input type="text" class="form-control" value="Funcionário" disabled="">
+                            </div>
+                          </div>
+                        <?php endif; ?>
+                        <div class="col-md-3">
+                          
+                          <div class="file-field input-field">
+                            
+                            <input type="file" name="fotoperfil">
+                            
+
+                          </div>
                         </div>
                       </div>
-                      <?php else:?>
-                      	<div class="col-md-12">
-                      	  <div class="form-group">
-                      	    <label class="bmd-label-floating">Tipo de Perfil</label>
-                      	    <input type="text" class="form-control" value="Funcionário" disabled="">
-                      	  </div>
-                      	</div>
-                      <?php endif; ?>
-                      <div class="col-md-3">
-                      
-    <div class="file-field input-field">
-      
-        <input type="file" name="fotoperfil">
-      
-
-    </div>
-    </div>
-    </div>
-              <button type="submit" class="btn btn-primary">Salvar Foto</button>
-              <div class="clearfix">
-                
-              </div>
-                  </form>
+                      <button type="submit" class="btn btn-primary">Salvar Foto</button>
+                      <div class="clearfix">
+                        
+                      </div>
+                    </form>
+                  </div>
                 </div>
               </div>
-            </div>
             </div>
           </div>
         </div>
@@ -153,7 +151,7 @@ The above copyright notice and this permission notice shall be included in all c
     </div>
   </div>
 
-<?php include '../assets/carregaJS.php'; ?>
+  <?php include '../assets/carregaJS.php'; ?>
 </body>
 
 </html>
